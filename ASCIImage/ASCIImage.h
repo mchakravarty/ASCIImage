d95854e67767d702db25d8bd499dff39b72bd880
Manuel M T Chakravarty <chak@justtesting.org>
2015-04-09 23:09:16 +1000

    Fix header name

--- ASCIImage/ASCIImage.h
+++ ASCIImage/ASCIImage.h
@@ -16,5 +16,5 @@ FOUNDATION_EXPORT const unsigned char ASCIImageVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ASCIImage/PublicHeader.h>
 
-#import <ASCIIImage/PARImage+ASCIInput.h>
+#import <ASCIImage/PARImage+ASCIIInput.h>
 

0a1f8fc33dbc1006ebbbcb7989b9dced43855bd8
Manuel M T Chakravarty <chak@justtesting.org>
2015-04-09 21:39:13 +1000

    Shared framework for OS X
    * Renamed ’ASCIImage’ (demo app) to ’ASCIImageDemo’
    * Created a new shared OS X framework target ’ASCIImage’

---
+++ ASCIImage/ASCIImage.h
@@ -0,0 +1,20 @@
+//
+//  ASCIImage.h
+//  ASCIImage
+//
+//  Created by Manuel M T Chakravarty on 9/04/2015.
+//  Copyright (c) 2015 Charles Parnot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ASCIImage.
+FOUNDATION_EXPORT double ASCIImageVersionNumber;
+
+//! Project version string for ASCIImage.
+FOUNDATION_EXPORT const unsigned char ASCIImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASCIImage/PublicHeader.h>
+
+#import <ASCIIImage/PARImage+ASCIInput.h>
+
